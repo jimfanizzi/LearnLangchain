5cd363ca40ceb5d580639b7ebebd71c560e83a81
jifanizz <jifanizz@cisco.com>
2024-02-19 19:45:45 -0500

    updates 4x

--- chat_with_document.py
+++ chat_with_document.py
@@ -67,13 +67,17 @@ def calculate_embedding_cost(texts):
     # print(f'Embedding Cost in USD: {total_tokens / 1000 * 0.00002:.6f}')
     return total_tokens, total_tokens / 1000 * 0.00002
 
+def clear_history():
+    if 'history' in st.session_state:
+        del st.session_state['history']
+
 
 if __name__ == "__main__":
     import os
     from dotenv import load_dotenv
     load_dotenv()
 
-    st.image('img.png')
+    st.image('resources/img.png')
     st.subheader('LLM Question-Answering Application 🤖 📚')
     with st.sidebar:
         # text_input for the OpenAI API key (alternative to python-dotenv and .env)
@@ -90,8 +94,10 @@ if __name__ == "__main__":
         # k number input widget
         k = st.number_input('k', min_value=1, max_value=20, value=3, on_change=clear_history)
 
+
         # add data button widget
         add_data = st.button('Add Data', on_click=clear_history)
+
 
         if uploaded_file and add_data: # if the user browsed a file
             with st.spinner('Reading, chunking and embedding file ...'):
@@ -115,5 +121,37 @@ if __name__ == "__main__":
                 # saving the vector store in the streamlit session state (to be persistent between reruns)
                 st.session_state.vs = vector_store
                 st.success('File uploaded, chunked and embedded successfully.')
+
+    # user's question text input widget
+    q = st.text_input('Ask a question about the content of your file:')
+    if q: # if the user entered a question and hit enter
+        standard_answer = "Answer only based on the text you received as input. Don't search external sources. " \
+                          "If you can't answer then return `I DONT KNOW`."
+        q = f"{q} {standard_answer}"
+        if 'vs' in st.session_state: # if there's the vector store (user uploaded, split and embedded a file)
+            vector_store = st.session_state.vs
+            st.write(f'k: {k}')
+            answer = ask_and_get_answer(vector_store, q, k)
+
+            # text area widget for the LLM answer
+            st.text_area('LLM Answer: ', value=answer)
+
+            st.divider()
+
+            # if there's no chat history in the session state, create it
+            if 'history' not in st.session_state:
+                st.session_state.history = ''
+
+            # the current question and answer
+            value = f'Q: {q} \nA: {answer}'
+
+            st.session_state.history = f'{value} \n {"-" * 100} \n {st.session_state.history}'
+            h = st.session_state.history
+
+            # text area widget for the chat history
+            st.text_area(label='Chat History', value=h, key='history', height=400)
+
+# run the app: streamlit run ./chat_with_documents.py
+
 
 
